83a4f77bd6f047186b68e038f502ee9ded7f5340
Abhi Uppal <abhinuv2000@gmail.com>
2021-11-07 15:10:12 -0800

    first likelihood calculation

--- thesis/optim.py
+++ thesis/optim.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
-import scipy as sp
 import time
 
 from math import pi
 from typing import Callable
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    import importlib_resources as pkg_resources
 
 
 def likelihood_t(A: np.array_equiv, B: np.array_equiv, x: np.array_equiv, mu: Callable):
@@ -14,9 +18,19 @@ def likelihood_t(A: np.array_equiv, B: np.array_equiv, x: np.array_equiv, mu: Ca
     BBT = np.matmul(B, B.T)
     Ax = np.matmul(A, x)
 
-    prod1 = np.matmul((x - mu(Ax)).T, BBT.inv)
+    prod1 = np.matmul((x - mu(Ax)).T, np.linalg.inv(BBT))
     prod2 = np.matmul(prod1, x - mu(Ax))
     L = n * np.log(2 * pi) + np.log(np.linalg.det(BBT)) + prod2
 
     return L
 
+
+if __name__ == "__main__":
+    AB = np.load("data/testdata/AB_1.npz")
+    A = AB["A"]
+    B = AB["B"]
+    data = pd.read_csv("data/testdata/data.csv")
+
+    x = np.array(data.iloc[1, 1:]).T
+
+    print(likelihood_t(A, B, x, mu=np.tanh))
